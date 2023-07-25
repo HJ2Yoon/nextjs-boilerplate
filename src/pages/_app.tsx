ffad7671130b44ac8dbe16e8be9d7996f2db10a8
HyeongJun Yoon <family72020@gmail.com>
2023-07-26 03:09:20 +0900

    [feat]: Layout 컴포넌트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppProps } from "next/app";
 import GlobalStyle from "styles/global-styles";
+import Layout from "components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
