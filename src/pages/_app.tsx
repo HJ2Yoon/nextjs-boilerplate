b979ad6b1dcf1f30e5ab1eea5d4b83a77af12525
HyeongJun Yoon <family72020@gmail.com>
2023-07-26 02:01:24 +0900

    [feat]: GlobalStyle 적용

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from "next/app";
+import GlobalStyle from "styles/global-styles";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
